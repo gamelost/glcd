9aec01dda9939dfbde5667a33fc435e9d1b86ddb
Greg Millam <captdeaf@gmail.com>
2014-06-05 18:00:50 -0700

    Add SendZones and SendZone.

--- glcd.go
+++ glcd.go
@@ -7,6 +7,8 @@ import (
 	// "github.com/gamelost/bot3server/server"
 	nsq "github.com/gamelost/go-nsq"
 	// irc "github.com/gamelost/goirc/client"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 	"log"
 	"os"
 	"os/signal"
@@ -46,6 +48,7 @@ type GLCClient struct {
 	Writer		*nsq.Writer
 	Heartbeat	time.Time
 	Clientid	string
+	State		interface{}		// json representation of player state.
 }
 
 // struct type for Bot3
@@ -58,6 +61,9 @@ type GLCD struct {
 	Clients			 map[string] *GLCClient
 
 	QuitChan                 chan os.Signal
+
+	MongoSession		*mgo.Session
+	MongoDB			*mgo.Database
 }
 
 func (glcd *GLCD) init(conf *iniconf.ConfigFile) error {
@@ -66,6 +72,27 @@ func (glcd *GLCD) init(conf *iniconf.ConfigFile) error {
 	glcd.Online = false
 
 	glcd.Clients = map[string]*GLCClient{}
+
+	// Connect to Mongo.
+	servers, err := glcd.ConfigFile.GetString("mongo", "servers")
+
+	if err != nil {
+		return fmt.Errorf("Mongo: No server configured.")
+	}
+
+	glcd.MongoSession, err = mgo.Dial(servers)
+
+	if err != nil {
+	}
+
+	db, err := glcd.ConfigFile.GetString("mongo", "db")
+
+	if err != nil {
+		return fmt.Errorf("Mongo: No database configured.")
+	}
+
+	glcd.MongoDB = glcd.MongoSession.DB(db)
+
 
 	lookupdAddress, _ := conf.GetString("nsq", "lookupd-address")
 	nsqdAddress, _ := conf.GetString("nsq", "nsqd-address")
@@ -92,8 +119,6 @@ func (glcd *GLCD) init(conf *iniconf.ConfigFile) error {
 	// anymore.
 	go glcd.CleanupClients()
 
-	// hardcoded kill command just in case
-
 	return nil
 }
 
@@ -102,12 +127,32 @@ func (cl *GLCClient) Publish(msg *Message) {
 		args := strings.SplitN(cl.Clientid, ":", 3)
 		if len(args) != 3 { return }
 		host, port, topic := args[0], args[1], args[2]
-		log.Printf("cl.publish: Attempting to connect to '" + host + ":" + port + "'")
+		// log.Printf("cl.publish: Attempting to connect to '" + host + ":" + port + "'")
 		cl.Writer = nsq.NewWriter(host + ":" + port)
 		cl.Topic = topic
 	}
 	encodedRequest, _ := json.Marshal(*msg)
 	cl.Writer.Publish(cl.Topic, encodedRequest)
+}
+
+func (cl *GLCClient) SendCommand(command string, data *Message) {
+	msg := Message{}
+	msg["command"] = command
+	msg["data"] = data
+	cl.Publish(&msg)
+}
+
+func (cl *GLCClient) SendMessage(text string) {
+	cl.SendCommand("message", &Message{"data": text})
+}
+
+func (glcd *GLCD) SendCommandAll(command string, data *Message) {
+	msg := Message{}
+	msg["command"] = command
+	msg["data"] = data
+	for _, v := range(glcd.Clients) {
+		v.Publish(&msg)
+	}
 }
 
 func (glcd *GLCD) CleanupClients() error {
@@ -123,7 +168,67 @@ func (glcd *GLCD) CleanupClients() error {
 	}
 }
 
-// Coming up next. COFFEE?
+// Send a zone file update.
+func (glcd *GLCD) SendZones(cl *GLCClient) {
+	c := glcd.MongoDB.C("zones")
+	q := c.Find(nil)
+
+	if q == nil {
+		cl.SendMessage(fmt.Sprintf("No zones found"))
+	} else {
+		var results []interface{}
+		err := q.All(&results)
+		if err == nil {
+			for _, res := range results {
+				cl.SendCommand("updateZone", &Message{"data": res})
+			}
+		} else {
+			cl.SendMessage(fmt.Sprintf("Unable to fetch zones: %v", err))
+		}
+	}
+}
+
+func (glcd *GLCD) SendZone(cl *GLCClient, zone string) {
+	c := glcd.MongoDB.C("zones")
+	query := bson.M{"zone":zone}
+	results := c.Find(query)
+
+	if results == nil {
+		cl.SendMessage(fmt.Sprintf("No such zone '%s'", zone))
+	} else {
+		var res interface{}
+		err := results.One(&res)
+		if err == nil {
+			cl.SendCommand("updateZone", &Message{"data": res})
+		} else {
+			cl.SendMessage(fmt.Sprintf("Unable to fetch zone: %v", err))
+		}
+	}
+}
+
+// Send a zone file update.
+func (glcd *GLCD) UpdateZone(cl *GLCClient, zone string, zdata interface{}) {
+	query := bson.M{"zone":zone}
+
+	c := glcd.MongoDB.C("zones")
+	val := bson.M{"type": "zone", "zdata": zdata, "timestamp": time.Now()}
+	change := bson.M{"$set": val}
+
+	err := c.Update(query, change)
+
+	if err == mgo.ErrNotFound {
+		val["id"], _ = c.Count()
+		change = bson.M{"$set": val}
+		err = c.Update(query, change)
+	}
+
+	if err != nil {
+		cl.SendMessage(fmt.Sprintf("Unable to update zone: %v", err))
+	} else {
+		cl.SendMessage(fmt.Sprintf("Updated zone '%s'", zone))
+	}
+}
+
 func (glcd *GLCD) HandleMessage(message *nsq.Message) error {
 	msg := Message{}
 
@@ -163,11 +268,49 @@ func (glcd *GLCD) HandleMessage(message *nsq.Message) error {
 	// If "client" from JSON is 
 	command, ok := cmddata.(string)
 	if !ok { return fmt.Errorf("Invalid format: command is not a string.") }
-	log.Printf("We got a command: ", command)
+	// log.Printf("We got a command: ", command)
 
 	switch command {
 	case "ping":
 		cl.Publish(&Message{"pong": fmt.Sprint(time.Now())})
+		break
+// 	case "playerState":
+// 		newstate, ok := msg["data"]
+// 		if ok {
+// 			cl.State = newstate
+// 			cl.Publish(&Message{"pong": fmt.Sprint(time.Now())})
+// 		}
+// 		break
+	case "updateZone":
+		args, ok := msg["data"]
+		if !ok { break }
+		margs, ok := args.(Message)
+		if !ok { break }
+		zonei, ok := margs["zone"]
+		if !ok { break }
+		zone, ok := zonei.(string)
+		if !ok { break }
+
+		zdata, ok := margs["data"]
+		if !ok { break }
+
+		glcd.UpdateZone(cl, zone, zdata)
+	case "sendZone":
+		args, ok := msg["data"]
+		if !ok { log.Printf("No data in sendZone"); break }
+		log.Println("data:")
+		log.Println(args)
+		margs, ok := args.(map[string]interface{})
+		if !ok { log.Printf("args is not a message in sendZone"); break }
+		zonei, ok := margs["zone"]
+		if !ok { log.Printf("No zone in sendZone"); break }
+		zone, ok := zonei.(string)
+		if !ok { log.Printf("Zone is not a string in sendZone"); break }
+
+		glcd.SendZone(cl, zone)
+		break
+	case "connected":
+		glcd.SendZones(cl)
 		break
 	case "wall":
 		for _, v := range(glcd.Clients) {
